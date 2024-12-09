74ef14e15d395f5a50fc117baf8ce9cf6fe14332
Iryna Osadcha <iryna.osadcha@gmail.com>
2024-12-09 15:37:13 -0500

    making -name optional

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -4,6 +4,6 @@
 import argparse 
 
 parser = argparse.ArgumentParser(description="Say hello")
-parser.add_argument('name',help='name to greet')
+parser.add_argument('-n', '--name', metavar='name', default='World', help='name to greet')
 args = parser.parse_args()
 print('Hello, ' + args.name + '!') 
